d68d72932cda1cd038b866ca7410c94d017f1f57
tomohiro.zoda <tomohiro.zoda@dena.com>
2016-04-20 23:41:33 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # prezzo
-presso のサービス停止を悼むあまり生み出されたクローン的なやつ
+presso のクローン的存在。
+
+<img src="http://img01.gatag.net/201504/15nrblp/gatag-00001396.png" width="150px;">
+
+## Installation
+
+```sh
+npm install
+```
+
+## Start Server
+```sh
+npm start
+```
+
+## Lint
+
+```sh
+npm run lint
+```
+
+## Contribution
+1. Fork it ( https://github.com/zdogma/prezzo/fork )
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
+
+## Author
+<img src="https://avatars3.githubusercontent.com/u/1973683?v=3&s=460" width="50px;" style="margin-right: 10px;">
+[z.dogma](https://github.com/zdogma)
---
+++ src/app/Main.js
@@ -0,0 +1,87 @@
+/**
+ * In this file, we create a React component
+ * which incorporates components providedby material-ui.
+ */
+
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+class Main extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  render() {
+    const standardActions = (
+      <FlatButton
+        label="Ok"
+        secondary={true}
+        onTouchTap={this.handleRequestClose}
+      />
+    );
+
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div style={styles.container}>
+          <Dialog
+            open={this.state.open}
+            title="残念でした"
+            actions={standardActions}
+            onRequestClose={this.handleRequestClose}
+          >
+          まだ何もできてないよ！
+          </Dialog>
+          <div>
+            <h1>prezzo</h1>
+            <h2>Hello World!</h2>
+            <img src="https://pbs.twimg.com/profile_images/685833126338166784/_ZjQGUzV_400x400.jpg" width="150px;"></img>
+          </div>
+          <RaisedButton
+            label="はじめる"
+            primary={true}
+            onTouchTap={this.handleTouchTap}
+          />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Main;
